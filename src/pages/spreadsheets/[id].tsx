fe6a9e45ecae54f7cadaba4ffbae0c4df5549a9f
Syncxv <aruldevarackal1@gmail.com>
2022-06-24 18:53:57 +0530

    fix talwind config

--- src/pages/spreadsheets/[id].tsx
+++ src/pages/spreadsheets/[id].tsx
@@ -2,8 +2,11 @@ import { NextPage } from 'next'
 
 const Spreadsheet: NextPage = () => {
     return (
-        <div className="wrapper">
-            <h1>spreadsheet</h1>
+        <div className="flex flex-col items-center h-screen">
+            <div className="w-full h-28 bg-gray-500"></div>
+            <div className="w-full h-full bg-gray-300">
+                <div className="cells"></div>
+            </div>
         </div>
     )
 }
