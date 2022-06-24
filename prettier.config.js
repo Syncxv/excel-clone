9d373ec1132e1fee8897756448eb2e207ea48600
Syncxv <aruldevarackal1@gmail.com>
2022-06-24 18:44:27 +0530

    init

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWidth: 4,
+    trailingComma: 'none',
+    printWidth: 120
+}
+
+module.exports = options
---
+++ src/pages/spreadsheets/[id].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const Spreadsheet: NextPage = () => {
+    return (
+        <div className="wrapper">
+            <h1>spreadsheet</h1>
+        </div>
+    )
+}
+
+export default Spreadsheet
