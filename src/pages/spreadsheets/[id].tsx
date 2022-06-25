c251f5f69a808ae2e98017a934a63e0c0ef8e830
Syncxv <aruldevarackal1@gmail.com>
2022-06-25 06:00:17 +0530

    sheeshers
    but theres probably a faster way than checking if EVERY letter is a
    why not just pop the first element and then render that speratly idk i cant spell btw

--- src/pages/spreadsheets/[id].tsx
+++ src/pages/spreadsheets/[id].tsx
@@ -7,23 +7,41 @@ export const Cell: FunctionalComponent<{ className?: string }> = ({ children, cl
     <div className={`cell w-24 h-5 border-gray-800 border-opacity-50 border ${className}`}>{children}</div>
 )
 
+const ColumnComponent: FunctionalComponent<{ col: Column }> = ({ col }) => (
+    <div key={col.letter} className={`${col.letter}`}>
+        <Cell className="flex items-center justify-center text-center bg-gray-300 font-semibold">{col.letter}</Cell>
+        {/* <div className="border-t border-gray-800 border-opacity-30"></div> */}
+        {col.cells.map(cell => (
+            <Cell key={cell.rowIndex}></Cell>
+        ))}
+    </div>
+)
+
 const Spreadsheet: NextPage = () => {
     const [grid, setGrid] = useState(createNewGrid())
     return (
         <div className="flex flex-col items-center h-screen">
             <div className="w-full h-28 "></div>
-            <div className="w-full h-full bg-gray-300 overflow-auto">
+            <div className="w-full h-full overflow-auto">
                 <div className="headThingy"></div>
                 <div className="cells flex">
-                    {grid.map(col => (
-                        <div key={col.letter} className={`${col.letter}`}>
-                            <Cell>{col.letter}</Cell>
-                            <div className="border-t border-gray-800 border-opacity-30"></div>
-                            {col.cells.map(cell => (
-                                <Cell key={cell.rowIndex}></Cell>
-                            ))}
-                        </div>
-                    ))}
+                    {grid.map(col =>
+                        col.letter === 'A' ? (
+                            <div className="flex">
+                                <div className="flex flex-col">
+                                    <Cell className="w-14"></Cell>
+                                    {col.cells.map(cell => (
+                                        <Cell className="flex items-center justify-center text-center bg-gray-300 w-14">
+                                            {cell.rowIndex}{' '}
+                                        </Cell>
+                                    ))}
+                                </div>
+                                <ColumnComponent col={col} />
+                            </div>
+                        ) : (
+                            <ColumnComponent col={col} />
+                        )
+                    )}
                 </div>
             </div>
         </div>
@@ -36,7 +54,7 @@ const createNewGrid = () => {
         const colLetter = String.fromCharCode(i)
         console.log('CURRENT COL LETTER: ', colLetter)
         const bruh: Column = { letter: colLetter, cells: [] }
-        for (let j = 0; j < 1000; ++j) {
+        for (let j = 0; j < 100; ++j) {
             bruh.cells.push(new CellClass(colLetter, j))
         }
         fucky.push(bruh)
