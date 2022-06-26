428cb19b06257173f349d8d9e877903c93e2eb62
Syncxv <aruldevarackal1@gmail.com>
2022-06-26 12:15:45 +0530

    WOAH

--- src/classes/Cell.ts
+++ src/classes/Cell.ts
@@ -1,30 +1,57 @@
-import { getId } from '../pages/spreadsheets/[id]'
-import { setEmptyImage } from '../util/setEmptyImage'
-import { sleep } from '../util/sleep'
+const letterMap: { [key: string]: number } = {
+    A: 0,
+    B: 1,
+    C: 2,
+    D: 3,
+    E: 4,
+    F: 5,
+    G: 6,
+    H: 7,
+    I: 8,
+    J: 9,
+    K: 10,
+    L: 11,
+    M: 12,
+    N: 13,
+    O: 14,
+    P: 15,
+    Q: 16,
+    R: 17,
+    S: 18,
+    T: 19,
+    U: 20,
+    V: 21,
+    W: 22,
+    X: 23,
+    Y: 24,
+    Z: 25
+}
 
 export class CellClass {
     columnLetter: string
     rowIndex: number
     selected: { isPrimary: boolean; isSelected: boolean }
-    constructor(columnLetter: string, rowIndex: number) {
+    height: number
+    ctx: CanvasRenderingContext2D
+    width: number
+    constructor(columnLetter: string, rowIndex: number, ctx: CanvasRenderingContext2D) {
         this.columnLetter = columnLetter
         this.rowIndex = rowIndex
+        this.ctx = ctx
         this.selected = { isPrimary: false, isSelected: false }
+        this.height = 10
+        this.width = 40
         this.onClick = this.onClick.bind(this)
-        this.onDragStart = this.onDragStart.bind(this)
-        // this.onDragOver = this.onDragOver.bind(this)
         this.initalize()
     }
-    get bruhTarget() {
-        return document.getElementById(getId(this))
-    }
 
-    async initalize() {
-        // if (this.bruhTarget == null) await sleep(20)
-        // Object.assign(this.bruhTarget!, { cell: this })
-        // this.bruhTarget!.addEventListener('click', this.onClick)
-        // this.bruhTarget!.addEventListener('dragstart', this.onDragStart)
-        // this.bruhTarget!.addEventListener('dragover', this.onDragOver)
+    async initalize() {}
+
+    draw() {
+        const { ctx } = this
+        ctx.beginPath()
+        ctx.rect(letterMap[this.columnLetter] * this.width, this.rowIndex * this.height, this.width, this.height)
+        ctx.stroke()
     }
 
     onClick() {
@@ -32,24 +59,11 @@ export class CellClass {
         this.select(true)
     }
 
-    onDragStart(e: DragEvent) {
-        // e.preventDefault()
-        // e.stopPropagation()
-        //WJY MNA WHY DOES IT NOT WORK WHEN I DO THAT FUCK
-        setEmptyImage(e)
-        unSelectAll()
-        this.select(true)
-        console.log(this)
-    }
-
     select(isPrimary: boolean = false) {
         this.selected = { isPrimary, isSelected: true }
-        this.bruhTarget?.classList.add(`selected${isPrimary ? '-primary' : ''}`)
     }
     unSelect() {
         this.selected = { isPrimary: false, isSelected: false }
-        this.bruhTarget?.classList.remove('selected-primary', 'selected')
-        console.log(this, this.bruhTarget)
     }
 }
 
--- src/classes/SheetManager.ts
+++ src/classes/SheetManager.ts
@@ -10,17 +10,21 @@ export class SheetManager {
 
     initalize() {
         this.canvas = document.getElementById('sheet') as HTMLCanvasElement
+        this.canvas.width = this.canvas.offsetWidth
+        this.canvas.height = this.canvas.offsetHeight
         this.ctx = this.canvas.getContext('2d')!
         this.frame = requestAnimationFrame(this.sheetLoop.bind(this))
         this.grid = this.newSheetGrid()
     }
 
     sheetLoop() {
+        const { ctx } = this
         this.frame = requestAnimationFrame(this.sheetLoop.bind(this))
-        this.ctx.fillStyle = 'rgb(0,0,0, 0.8)'
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+        this.grid
+            .map(s => s.cells)
+            .flat()
+            .forEach(s => s.draw())
     }
-
     newSheetGrid() {
         let fucky: Column[] = []
         for (let i = 65; i < 91; ++i) {
@@ -28,7 +32,7 @@ export class SheetManager {
             console.log('CURRENT COL LETTER: ', colLetter)
             const bruh: Column = { letter: colLetter, cells: [] }
             for (let j = 0; j < 100; ++j) {
-                bruh.cells.push(new CellClass(colLetter, j))
+                bruh.cells.push(new CellClass(colLetter, j, this.ctx))
             }
             fucky.push(bruh)
         }
